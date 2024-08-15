e469f964ec452b13a17948616170fcae9f078859
Mariam Ahmed <51763380+MariamFahmy@users.noreply.github.com>
2024-08-15 11:07:43 -0230

    Web Components

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from './services/Menu.js';
 import Router from './services/Router.js';
+// Link my Web Components
+import { MenuPage } from './components/MenuPage.js';
+import { DetailsPage } from './components/DetailsPage.js'
+import { OrderPage } from './components/OrderPage.js'
+
 
 window.app = {}
 app.store = Store; // one store for whole app, making Store global
